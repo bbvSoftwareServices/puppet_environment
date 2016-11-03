86fe3d15ce98b610624c5dd1e9e6b0370b5317e0
Gerald Mixa <Gerald.Mixa@bbv.eu>
2016-11-03 10:17:11 +0100

    Korrektur Doku

--- bootstrap/puppet.sh
+++ bootstrap/puppet.sh
@@ -2,8 +2,6 @@
 
 export PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
-# Bootstrap puppet on Debian 8 (Jessie) with the Debian 7 (Wheezy)
-# release repo from puppetlabs, because Jessies repo is empty (20151221).
 # Bootstrap apt-transport-https use for debian repositories via https
 apt-get install -y apt-transport-https
 # Install mc git joe
@@ -17,14 +15,6 @@ if ! dpkg -s puppet >/dev/null 2>&1; then
     fi
 	apt-get update &&
     apt-get -y install puppet-agent puppetserver puppetdb puppetdb-termini </dev/null
-    # Remove deprecated templatedir directive in puppet.conf (comes from deb itself)
-    # sed -i '/^templatedir/d' /etc/puppet/puppet.conf
-    # Remove unused default factpath directive causing warnings
-    # sed -i '/^factpath/d' /etc/puppet/puppet.conf
 fi
 apt-get -y upgrade
-
-
-
-
 # EOF
